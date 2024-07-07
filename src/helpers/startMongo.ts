9f8076dcfce32ebccec54b901f40c9ec8303e901
red_somber <red@somber.com>
2024-07-07 15:41:28 +0500

    startMongo edit

--- src/helpers/startMongo.ts
+++ src/helpers/startMongo.ts
@@ -1,8 +1,27 @@
 import { connect } from 'mongoose'
 import env from '@/helpers/env'
 
-function startMongo() {
-  return connect(env.MONGO)
+async function startMongo() {
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+  }
+
+  const connectWithRetry = async () => {
+    try {
+      await connect(env.MONGO, options)
+      console.log('MongoDB connected successfully')
+    } catch (error) {
+      console.error(
+        'Failed to connect to MongoDB, retrying in 5 seconds...',
+        error
+      )
+      setTimeout(connectWithRetry, 5000)
+    }
+  }
+
+  await connectWithRetry()
 }
 
 export default startMongo
